78348fba179582712c1dc241ee5b3ce449ccfec0
Brian Whitsett <56644229+Bwhitsett@users.noreply.github.com>
2022-04-13 13:42:45 -0400

    Window Update

--- components/News.js
+++ components/News.js
@@ -106,6 +106,7 @@ const styles = StyleSheet.create({
         color: '#fff',
         fontSize: 50,
         alignSelf: 'center',
+        marginBottom: 10,
     },
     newsStoryText: {
         color: '#fff',
